f46dcd19f5ee57f152a8f288529750f60a007a08
Pranay <apple@Pranays-MacBook-Air.local>
2017-11-18 01:19:49 +0530

    removing resp on error

--- src/modules/common/filters/DispatchError.ts
+++ src/modules/common/filters/DispatchError.ts
@@ -9,31 +9,21 @@ import { ValidationError } from 'sequelize';
 export class DispatchError implements ExceptionFilter {
     public catch(err, res) {
         if (err instanceof MessageCodeError) {
-            console.log("coming here");
             console.log(err);
             /* MessageCodeError, Set all header variable to have a context for the client in case of MessageCodeError. */
             res.setHeader('x-message-code-error', err.messageCode);
             res.setHeader('x-message', err.message);
             res.setHeader('x-httpStatus-error', err.httpStatus);
 
-            let data = {
-                "status" : 0,
-                "message" : err.message
-            }
-
-            return res.status(err.httpStatus).send(data);
+            return res.status(err.httpStatus).send();
         } else if (err instanceof ValidationError) {
+            console.log(err);
             /* Sequelize validation error. */
             res.setHeader('x-message-code-error', (err as ValidationError).errors[0].type);
             res.setHeader('x-message', (err as ValidationError).errors[0].message);
             res.setHeader('x-httpStatus-error', HttpStatus.BAD_REQUEST);
 
-            let data = {
-                "status" : 0,
-                "message" : (err as ValidationError).errors[0].message
-            }
-
-            return res.status(HttpStatus.BAD_REQUEST).send(data);
+            return res.status(HttpStatus.BAD_REQUEST).send();
         } else {
             return res.status(HttpStatus.INTERNAL_SERVER_ERROR).send();
         }
